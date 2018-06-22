d215ad530e617d9894ac1f3aa97cff5171d9ec17
Herod, Kathryn <codingkat7@gmail.com>
2018-06-21 22:48:25 -0500

    testing set up with basic functions for App Component

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+
+describe('App Component', () => {
+  it('shows the comment box', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+
+    //this clears the element(s) rendered to free up testing memory
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
