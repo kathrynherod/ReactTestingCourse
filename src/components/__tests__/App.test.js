c2e71707db67860d685049ba5fc028237a7227c9
Herod, Kathryn <codingkat7@gmail.com>
2018-06-21 22:52:07 -0500

    implemented comment box test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -6,7 +6,7 @@ describe('App Component', () => {
   it('shows the comment box', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
-
+    expect(div.innerHTML).toContain('Comment Box');
     //this clears the element(s) rendered to free up testing memory
     ReactDOM.unmountComponentAtNode(div);
   });
